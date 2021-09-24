97ddd22f69db1f4bfd652d263bbbf632614d49e1
Ichhwak Malhare <ichwakmalhare@gmail.com>
2021-09-24 15:13:09 +0530

    Update AGLabs.java

--- AGLabs.java
+++ AGLabs.java
@@ -1,28 +1,21 @@
-import java.util.*; 
+import java.util.*;
 import java.io.*;
 
-class Aglabs {
-
-  
-
-    public static boolean TreeConstructor(String[] a){
-	    int im=0,i2=0;
-	    for(int i=0;i<a.length;i++){
-	        int i1=(int)a[i].charAt(1);
-	        if(im<i1){
-	            im=i1;
-	        }
-	        else{
-	            return false;
-	        }
-	    }
-	    return true;
-	}
-  
-
-  public static void main (String[] args) {  
-    Scanner s = new Scanner(System.in);
-    System.out.print(TreeConstructor(s.nextLine())); 
+class AGLabs {
+  public static boolean TreeConstructor(String[] arr) {
+    int in = 0, i2 = 0;
+    for (int i = 0; i < arr.length; i++) {
+      int i1 = (int) arr[i].charAt(1);
+      if ( in < i1)
+        in = i1;
+      else
+        return false;
+    }
+    return true;
   }
 
+  public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      System.out.print(TreeConstructor(s.nextLine()));
+  }
 }
